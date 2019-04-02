6efa6f3d8ac25f3b43ff7c8aeb01fd3351882d39
tuhe32 <tuhe32@163.com>
2019-04-02 15:57:47 +0800

    111

--- meta.js
+++ meta.js
@@ -5,6 +5,7 @@ const {
   installDependencies,
   runLintFix,
   printMessage,
+  cdAndRun
 } = require('./utils')
 
 module.exports={
@@ -75,8 +76,11 @@ module.exports={
             // .then(() => {
             //   return runLintFix(cwd, data, green)
             // })
+            // .then(() => {
+            //   printMessage(data, green)
+            // })
             .then(() => {
-              printMessage(data, green)
+              return cdAndRun(cwd,data,green)
             })
             .catch(e => {
               console.log(chalk.red('Error:'), e)
--- utils/index.js
+++ utils/index.js
@@ -23,6 +23,18 @@ exports.installDependencies = function installDependencies(
 }
 
 /**
+ * Runs `npm run dev` in the project directory
+ * @param {string} cwd Path of the created project directory
+ * @param {object} data Data from questionnaire
+ */
+exports.cdAndRun = function cdAndRun(cwd, data, color) {
+  console.log(`\n\n# ${color('开始启动项目中，别动！ ...')}`)
+  console.log('# ========================\n')
+  runCommand('cd',[data.destDirName],{cwd});
+  return runCommand(data.autoInstall,['run dev'],{cwd})
+}
+
+/**
  * Runs `npm run lint -- --fix` in the project directory
  * @param {string} cwd Path of the created project directory
  * @param {object} data Data from questionnaire
